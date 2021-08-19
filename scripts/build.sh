3857449cbea83a1822deb4046d9f26df7bf25bc6
Saiteju1997 <51901826+Saiteju1997@users.noreply.github.com>
2021-08-19 10:52:28 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 gh-token=$1
 app_version=$2
-echo "$gh-token" > /tmp/token
+echo $gh-token > /tmp/token
 gh auth login --with-token < /tmp/token
 gh pr create --title "Weekly release" -b "Updated the Version file" -r "Saiteju1997" -B "main" -H "release-v$app_version"
